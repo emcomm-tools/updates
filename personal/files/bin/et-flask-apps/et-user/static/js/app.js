126e616226cbbd0165e92a64b044e9c8db31a91e
Sylvain Deguire <sylvain@va2ops.ca>
2026-02-01 19:34:51 -0500

    change in save

--- personal/files/bin/et-flask-apps/et-user/static/js/app.js
+++
@@ -1,8 +0,0 @@
-/*
- * et-user - User Configuration JavaScript
- * Author: Sylvain Deguire (VA2OPS)
- * Date: January 2026
- */
-
-// Placeholder - main logic is in the template
-console.log('et-user loaded');
